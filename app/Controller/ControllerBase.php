248b4c5e0cc906873f24f51ceae3e894088d4ede
Taufan Aditya <toopay@taufanaditya.com>
2013-02-02 00:06:54 +0700

    Refactor synchronize script

--- app/Controller/ControllerBase.php
+++ app/Controller/ControllerBase.php
@@ -43,7 +43,7 @@ class ControllerBase
 
 		// TODO : More filtering
 		if (strpos($userAgent, 'curl') !== FALSE) {
-			passthru('cd ../phpindonesia;git checkout develop;git pull origin develop;git checkout master;git merge develop;git push origin master');
+			passthru('cd ../phpindonesia;git checkout master;git fetch origin;git merge origin/develop;git push origin master');
 		}
 
 		return $this->render('OK'."\n");
