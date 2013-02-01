9d95a504e8fa846762f19b07a9a96268e7c98c63
Taufan Aditya <toopay@taufanaditya.com>
2013-02-01 16:15:10 +0700

    Add code-coverage annotation for non-Class file

--- app/routes.php
+++ app/routes.php
@@ -7,34 +7,16 @@
  */
 
 // @codeCoverageIgnoreStart
-use Symfony\Component\Routing\Route;
+use app\Route;
 use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 $routes = new RouteCollection();
 
-$routes->add('default', new Route('/', array('_controller' =>
-    function (Request $request) {
-        return new Response('You are in default route!');
-    }
-)));
+$routes->add('default', new Route('/'));
 
-$routes->add('universal_route_default', new Route('/{controller}', array('_controller' =>
-    function (Request $request) {
-        return new Response('You are in universal route controller!');
-    }
-), array('controller' => '[a-z]+')));
+$routes->add('universal_route_default', new Route('/{controller}', array('controller' => '[a-z]+')));
 
-$routes->add('universal_route_action', new Route('/{controller}/{action}', array('_controller' =>
-    function (Request $request) {
-        return new Response('You are in universal route controller/action!');
-    }
-), array('controller' => '[a-z]+', 'action' => '[a-z]+')));
+$routes->add('universal_route_action', new Route('/{controller}/{action}', array('controller' => '[a-z]+', 'action' => '[a-z]+')));
 
-$routes->add('universal_route_action_id', new Route('/{controller}/{action}/{id}', array('_controller' =>
-    function (Request $request) {
-        return new Response('You are in universal route controller/action/id!');
-    }
-), array('controller' => '[a-z]+', 'action' => '[a-z]+', 'id' => '[0-9]+')));
+$routes->add('universal_route_action_id', new Route('/{controller}/{action}/{id}', array('controller' => '[a-z]+', 'action' => '[a-z]+', 'id' => '[0-9]+')));
 // @codeCoverageIgnoreEnd
