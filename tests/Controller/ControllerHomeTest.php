48be929ebbcd076c5d027db21e91bcb3f187824b
Taufan Aditya <toopay@taufanaditya.com>
2013-02-01 20:04:28 +0700

    Test suite skeleton

---
+++ tests/Controller/ControllerHomeTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the PHP Indonesia package.
+ *
+ * (c) PHP Indonesia 2013
+ */
+
+use app\Controller\ControllerHome;
+use Symfony\Component\HttpFoundation\Request;
+
+class ControllerHomeTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Cek konsistensi controller base instance
+	 */
+	public function testCekKonsistensiAppControllerHome()
+	{
+		$request = Request::create('/home/index');
+		$controllerHome = new ControllerHome($request);
+
+		$this->assertInstanceOf('\app\Controller\ControllerBase', $controllerHome);
+		$this->assertInstanceOf('\app\Controller\ControllerHome', $controllerHome);
+		$this->assertObjectHasAttribute('request', $controllerHome);
+	}
+
+	/**
+	 * Cek action index
+	 */
+	public function testCekActionIndexAppControllerHome()
+	{
+		$request = Request::create('/home/index');
+		$controllerHome = new ControllerHome($request);
+		$response = $controllerHome->actionIndex();
+
+		$this->assertInstanceOf('\Symfony\Component\HttpFoundation\RedirectResponse', $response);
+		$this->assertEquals(302, $response->getStatusCode());
+	}
+
+	/**
+	 * Cek action foo
+	 */
+	public function testCekActionFooAppControllerHome()
+	{
+		$request = Request::create('/home/index');
+		$controllerHome = new ControllerHome($request);
+		$response = $controllerHome->actionFoo();
+
+		$this->assertInstanceOf('\Symfony\Component\HttpFoundation\Response', $response);
+		$this->assertEquals(200, $response->getStatusCode());
+		$this->assertEquals('You are in Bar ', $response->getContent());
+	}
+}
---
+++ tests/ControllerKernelTest.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ * This file is part of the PHP Indonesia package.
+ *
+ * (c) PHP Indonesia 2013
+ */
+
+use app\Route;
+use app\ControllerKernel;
+use Symfony\Component\HttpFoundation\Request;
+
+class ControllerKernelTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Cek konsistensi controller kernel instance
+	 */
+	public function testCekKonsistensiAppControllerKernel()
+	{
+		$controllerKernel = new ControllerKernel(array(
+			Route::HANDLER_CLASS => 'home',
+			Route::HANDLER_ACTION => 'index',
+		));
+
+		$this->assertInstanceOf('\app\ControllerKernelInterface', $controllerKernel);
+	}
+
+	/**
+	 * Cek invalid handler [KOSONG]
+	 */
+	public function testCekInvalidHandlerKosong()
+	{
+		$this->setExpectedException('Exception', 'Invalid handler for this request');
+
+		$controllerKernel = new ControllerKernel(array());
+	}
+
+	/**
+	 * Cek invalid handler [ADA TAPI TIDAK VALID]
+	 */
+	public function testCekInvalidHandlerParameterKey()
+	{
+		$request = Request::create('/home/index');
+		$controllerKernel = new ControllerKernel(array(
+			Route::HANDLER_CLASS => NULL,
+			Route::HANDLER_ACTION => '',
+		));
+
+		$this->setExpectedException('Exception', 'Cannot find handler for this request');
+
+		$controllerKernel->handle($request);
+	}
+
+	/**
+	 * Cek valid handler dan invalid controller parameter
+	 */
+	public function testCekValidHandlerInvalidControllerParameter()
+	{
+		$request = Request::create('/home/index');
+		$controllerKernel = new ControllerKernel(array(
+			Route::HANDLER_CLASS => 'home',
+			Route::HANDLER_ACTION => 'actionyangtidakada',
+		));
+
+		$this->setExpectedException('Exception', 'Cannot find action for this request');
+
+		$controllerKernel->handle($request);
+	}
+
+	/**
+	 * Cek valid handler dan valid controller
+	 */
+	public function testCekValidHandlerValidControllerParameter()
+	{
+		$request = Request::create('/home/index');
+		$controllerKernel = new ControllerKernel(array(
+			Route::HANDLER_CLASS => 'home',
+			Route::HANDLER_ACTION => 'index',
+		));
+
+		$response = $controllerKernel->handle($request);
+
+		$this->assertInstanceOf('\Symfony\Component\HttpFoundation\Response', $response);
+	}
+}
