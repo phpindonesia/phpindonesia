8b45bb0bacb7f30f238b04974e865e954ccf6254
nurcahyo <2light.hidayah@gmail.com>
2013-02-06 12:45:11 +0700

    invoke assetmanager

--- app/Controller/ControllerBase.php
+++ app/Controller/ControllerBase.php
@@ -11,82 +11,108 @@ namespace app\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Assetic\FilterManager;
+use Assetic\Filter\LessphpFilter;
+use Assetic\Factory\AssetFactory;
+use Assetic\Extension\Twig\AsseticExtension;
+use Assetic\Factory\LazyAssetManager;
+use Assetic\Extension\Twig\TwigFormulaLoader;
+use Assetic\Extension\Twig\TwigResource;
+use Assetic\AssetWriter;
 
 /**
  * ControllerBase
  *
  * @author PHP Indonesia Dev
  */
-class ControllerBase 
-{
-	protected $request;
+class ControllerBase {
 
-	/**
-	 * Constructor.
-	 *
-	 * @param Request $request A Request instance
-	 */
-	public function __construct(Request $request)
-	{
-		$this->request = $request;
-	}
+    protected $request;
+    public $layout = 'layout.html.tpl';
 
-	/**
-	 * This is internally used by Travis-CI (JANGAN DIUTAK ATIK :)
-	 *
-	 * @codeCoverageIgnore
-	 * @see app/routes.php
-	 */
-	public function actionSynchronize()
-	{
-		$userAgent = $this->request->server->get('HTTP_USER_AGENT');
+    /**
+     * Constructor.
+     *
+     * @param Request $request A Request instance
+     */
+    public function __construct(Request $request) {
+        $this->request = $request;
+    }
 
-		// TODO : More filtering
-		if (strpos($userAgent, 'curl') !== FALSE) {
-			passthru('cd ../phpindonesia;git checkout master;git fetch origin;git merge origin/develop;git push origin master');
-		}
+    /**
+     * This is internally used by Travis-CI (JANGAN DIUTAK ATIK :)
+     *
+     * @codeCoverageIgnore
+     * @see app/routes.php
+     */
+    public function actionSynchronize() {
+        $userAgent = $this->request->server->get('HTTP_USER_AGENT');
 
-		return $this->render('OK'."\n");
-	}
+        // TODO : More filtering
+        if (strpos($userAgent, 'curl') !== FALSE) {
+            passthru('cd ../phpindonesia;git checkout master;git fetch origin;git merge origin/develop;git push origin master');
+        }
 
-	/**
-	 * Redirect response.
-	 *
-	 * @param string $url URL Path
-	 * @param int $status HTTP status code
-	 * @param Array $headers HTTP Headers
-	 *
-	 * @return RedirectResponse A Response instance
-	 */
-	public function redirect($url = '', $status = 302, $headers = array())
-	{
-		return new RedirectResponse($url, $status, $headers);
-	}
+        return $this->render('OK' . "\n");
+    }
 
-	/**
-	 * Render response.
-	 *
-	 * @param mixed $content A Response content
-	 * @param int $status HTTP status code
-	 * @param Array $headers HTTP Headers
-	 *
-	 * @return Response A Response instance
-	 */
-	public function render($data = NULL, $status = 200, $headers = array())
-	{
-		// Persiapkan data yang akan di-render
-		if (is_array($data) && array_key_exists('title', $data) && array_key_exists('content', $data)) {
-			// Data yang dikirim memiliki parameter minimal yakni 'title' dan 'content'
-			// yang diperlukan Twig. Load template yang berkaitan dan assign data.
-			$twigLoader = new \Twig_Loader_Filesystem(APPLICATION_PATH . DIRECTORY_SEPARATOR . 'Templates');
-			$twig = new \Twig_Environment($twigLoader);
-			$content = $twig->render('layout.html.tpl', $data);
-		} else {
-			// Data yang dikirim tidak memenuhi kriteria
-			// Outputkan raw data
-			$content = $data;
-		}
+    /**
+     * Redirect response.
+     *
+     * @param string $url URL Path
+     * @param int $status HTTP status code
+     * @param Array $headers HTTP Headers
+     *
+     * @return RedirectResponse A Response instance
+     */
+    public function redirect($url = '', $status = 302, $headers = array()) {
+        return new RedirectResponse($url, $status, $headers);
+    }
 
-		return new Response($content, $status, $headers);
-	}
+    /**
+     * Render response.
+     *
+     * @param mixed $content A Response content
+     * @param int $status HTTP status code
+     * @param Array $headers HTTP Headers
+     *
+     * @return Response A Response instance
+     */
+    public function render($data = NULL, $status = 200, $headers = array()) {
+        // Persiapkan data yang akan di-render
+        if (is_array($data) && array_key_exists('title', $data) && array_key_exists('content', $data)) {
+            // Data yang dikirim memiliki parameter minimal yakni 'title' dan 'content'
+            // yang diperlukan Twig. Load template yang berkaitan dan assign data.
+            $twigLoader = new \Twig_Loader_Filesystem(APPLICATION_PATH . DIRECTORY_SEPARATOR . 'Templates');
+            $twig = new \Twig_Environment($twigLoader);
+            $this->invokeAssetic($twig);
+            $content = $twig->render($this->layout, $data);
+        } else {
+            // Data yang dikirim tidak memenuhi kriteria
+            // Outputkan raw data
+            $content = $data;
+        }
+
+        return new Response($content, $status, $headers);
+    }
+
+    protected function invokeAssetic(\Twig_Environment $twig) {
+        $fm = new FilterManager();
+        $fm->set('less', new LessphpFilter());
+        $factory = new AssetFactory(ASSET_FACTORY_PATH);
+        $factory->setFilterManager($fm);
+        $twig->addExtension(new AsseticExtension($factory));
+
+        $am = new LazyAssetManager($factory);
+        // enable loading assets from twig templates
+        $am->setLoader('twig', new TwigFormulaLoader($twig));
+        // loop through all your templates
+        $resource = new TwigResource($twig->getLoader(), $this->layout);
+        $am->addResource($resource, 'twig');
+        if (APPLICATION_DEBUG) {
+            $writer = new AssetWriter(BASE_PATH);
+            $writer->writeManagerAssets($am);
+        }
+    }
+
 }
--- app/Templates/layout.html.tpl
+++ app/Templates/layout.html.tpl
@@ -1,13 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8"/>
-  <title>{{ title }}</title>
+    <head>
+        <meta charset="utf-8"/>
+        <title>{{ title }}</title>
 
-  <!-- Mobile viewport optimisation -->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-{{ content }}
-</body>
+        <!-- Mobile viewport optimisation -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        {% stylesheets 'bootstrap/less/bootstrap.less' filter='less' output='/assets/css/main.css' %}
+        <link href="{{ asset_url }}" type="text/css" rel="stylesheet" />
+        {% endstylesheets %}
+    </head>
+    <body>
+        {{ content }}
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+        {% javascripts 
+'bootstrap/js/bootstrap-alert.js'
+'bootstrap/js/bootstrap-modal.js' 
+'bootstrap/js/bootstrap-dropdown.js' 
+'bootstrap/js/bootstrap-scrollspy.js' 
+'bootstrap/js/bootstrap-tab.js' 
+'bootstrap/js/bootstrap-tooltip.js' 
+'bootstrap/js/bootstrap-popover.js' 
+'bootstrap/js/bootstrap-button.js' 
+'bootstrap/js/bootstrap-collapse.js' 
+'bootstrap/js/bootstrap-carousel.js' 
+'bootstrap/js/bootstrap-typeahead.js' 
+'bootstrap/js/bootstrap-affix.js' output="/assets/js/bootstrap.js" %}
+        <script type="text/javascript" src="{{ asset_url }}"></script>
+    {% endjavascripts %}
+    </body>
 </html>
