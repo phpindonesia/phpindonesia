a9e26fe82744c83f9bd0ea4205083d940ecd20f0
Taufan Aditya <toopay@taufanaditya.com>
2013-02-22 11:24:53 +0700

    Placeholder to tooltip

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,17 @@
 $(function () {
 
-	// SLIDER
-	//$('#slider').carousel();
+	// PLACEHOLDER TIP
+	$(':input').focusin(function(){
+		if ($(this).attr('type') != 'submit') {
+			$(this).tooltip({
+				placement: 'right',
+				title: $(this).attr('placeholder'),
+			});
+			$(this).tooltip('show');
+		}
+	});
 
+	$(':input').focusout(function(){
+		$(this).tooltip('hide');
+	});
 });
