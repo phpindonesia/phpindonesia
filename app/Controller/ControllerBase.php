511c5c08028e12d01dccc2bd468384fff0dc6ee7
Taufan Aditya <toopay@taufanaditya.com>
2013-02-23 11:16:08 +0700

    Refactor render, move it to template model domain

--- app/Controller/ControllerBase.php
+++ app/Controller/ControllerBase.php
@@ -24,212 +24,210 @@ use app\Model\ModelBase;
  */
 class ControllerBase {
 
-    protected $request;
-    protected $session;
-    protected $acl;
-    protected $data;
-    protected $layout = 'layout.tpl';
+	protected $request;
+	protected $session;
+	protected $acl;
+	protected $data;
+	protected $layout = 'layout.tpl';
 
-     /**
-     * This is internally used by Travis-CI (JANGAN DIUTAK ATIK :)
-     *
-     * @codeCoverageIgnore
-     * @see app/routes.php
-     */
-    public function actionSynchronize() {
-        $userAgent = $this->request->server->get('HTTP_USER_AGENT');
+	 /**
+	 * This is internally used by Travis-CI (JANGAN DIUTAK ATIK :)
+	 *
+	 * @codeCoverageIgnore
+	 * @see app/routes.php
+	 */
+	public function actionSynchronize() {
+		$userAgent = $this->request->server->get('HTTP_USER_AGENT');
 
-        // TODO : More filtering
-        if (strpos($userAgent, 'curl') !== FALSE) {
-            passthru('cd ../phpindonesia;git checkout master;git fetch origin;git merge origin/develop;git push origin master');
-        }
+		// TODO : More filtering
+		if (strpos($userAgent, 'curl') !== FALSE) {
+			passthru('cd ../phpindonesia;git checkout master;git fetch origin;git merge origin/develop;git push origin master');
+		}
 
-        return $this->render('OK' . "\n");
-    }
+		return $this->render('OK' . "\n");
+	}
 
-    /**
-     * Constructor.
-     *
-     * @param Request $request A Request instance
-     */
-    public function __construct(Request $request) {
-        $this->request = $request;
+	/**
+	 * Constructor.
+	 *
+	 * @param Request $request A Request instance
+	 */
+	public function __construct(Request $request) {
+		$this->request = $request;
 
-        // Initialize the session
-        if ( ! $this->request->hasPreviousSession()) {
-            $this->session = new Session();
-            $this->session->start();
-            $this->request->setSession($this->session);
-        } else {
-            // @codeCoverageIgnoreStart
-            $this->session = $this->request->getSession();
-            // @codeCoverageIgnoreEnd
-        }
+		// Initialize the session
+		if ( ! $this->request->hasPreviousSession()) {
+			$this->session = new Session();
+			$this->session->start();
+			$this->request->setSession($this->session);
+		} else {
+			// @codeCoverageIgnoreStart
+			$this->session = $this->request->getSession();
+			// @codeCoverageIgnoreEnd
+		}
 
-        // Initialize Acl and Data instances
-        $this->acl = new Acl($this->request);
-        $this->data = new Parameter();
+		// Initialize Acl and Data instances
+		$this->acl = new Acl($this->request);
+		$this->data = new Parameter();
 
-        // Before action hook
-        if (is_callable(array($this,'beforeAction'))) {
-            $this->beforeAction();
-        }
-    }
+		// Before action hook
+		if (is_callable(array($this,'beforeAction'))) {
+			$this->beforeAction();
+		}
+	}
 
-    /**
-     * beforeAction hook (Global)
-     */
-    public function beforeAction() {
-        // Assign acl object
-        $this->data->set('acl', $this->acl);
+	/**
+	 * beforeAction hook (Global)
+	 */
+	public function beforeAction() {
+		// Assign acl object
+		$this->data->set('acl', $this->acl);
 
-        if ($this->acl->isLogin()) {
-            // Assign user data
-            $this->data->set('user', ModelBase::factory('Auth')->getUser($this->session->get('userId')));
-        }
+		if ($this->acl->isLogin()) {
+			// Assign user data
+			$this->data->set('user', ModelBase::factory('Auth')->getUser($this->session->get('userId')));
+		}
 
-        // Assign POST data
-        if ($_POST || $this->session->get('postData')) {
-            $postData = array();
+		// Assign POST data
+		if ($_POST || $this->session->get('postData')) {
+			$postData = array();
 
-            if ($_POST) $postData = array_merge($postData, $_POST);
+			if ($_POST) $postData = array_merge($postData, $_POST);
 
-            if ($this->session->get('postData')) {
-                $postData = array_merge($postData, $this->session->get('postData'));
-                // Unset
-                $this->session->set('postData', NULL);
-            }
+			if ($this->session->get('postData')) {
+				$postData = array_merge($postData, $this->session->get('postData'));
+				// Unset
+				$this->session->set('postData', NULL);
+			}
 
-            $this->data->set('postData', $postData);
-        }
+			$this->data->set('postData', $postData);
+		}
 
-        // Assign GET data
-        if ($_GET) {
-            $this->data->set('getData', $_GET);
-        }
+		// Assign GET data
+		if ($_GET) {
+			$this->data->set('getData', $_GET);
+		}
 
-        // Detect path information
-        $currentUrl = $this->request->server->get('PATH_INFO');
+		// Detect path information
+		$currentUrl = $this->request->server->get('PATH_INFO');
 
-        // Try workaround on CGI environment
-        if (empty($currentUrl)) $currentUrl = $this->request->server->get('SCRIPT_URL');
+		// Try workaround on CGI environment
+		if (empty($currentUrl)) $currentUrl = $this->request->server->get('SCRIPT_URL');
 
-        if ( ! empty($currentUrl)) {
-            $queryUrl = $_GET;
-            
-            // Exceptions for this keys
-            $postToGetKeys = array('query');
-            $flashedKeys = array('page', $currentUrl);
+		if ( ! empty($currentUrl)) {
+			$queryUrl = $_GET;
+			
+			// Exceptions for this keys
+			$postToGetKeys = array('query');
+			$flashedKeys = array('page', $currentUrl);
 
-            // add any detected keys that match exception elements
-            foreach ($postToGetKeys as $postToGetKey) {
-                if (isset($_POST[$postToGetKey])) $queryUrl[$postToGetKey] = $_POST[$postToGetKey];
-            }
+			// add any detected keys that match exception elements
+			foreach ($postToGetKeys as $postToGetKey) {
+				if (isset($_POST[$postToGetKey])) $queryUrl[$postToGetKey] = $_POST[$postToGetKey];
+			}
 
-            // remove any flashed keys
-            foreach($flashedKeys as $flashedKey) {
-                if (isset($queryUrl[$flashedKey])) unset($queryUrl[$flashedKey]);
-            }
+			// remove any flashed keys
+			foreach($flashedKeys as $flashedKey) {
+				if (isset($queryUrl[$flashedKey])) unset($queryUrl[$flashedKey]);
+			}
 
-            $currentQueryUrl = $currentUrl.'?';
+			$currentQueryUrl = $currentUrl.'?';
 
-            if ( ! empty($queryUrl)) {
-                $currentQueryUrl .= http_build_query($queryUrl).'&';
-            }
+			if ( ! empty($queryUrl)) {
+				$currentQueryUrl .= http_build_query($queryUrl).'&';
+			}
 
-            // Set common URL variable
-            $this->data->set('currentUrl', $currentUrl);
-            $this->data->set('currentQueryUrl', $currentQueryUrl);
-        }
-    }
+			// Set common URL variable
+			$this->data->set('currentUrl', $currentUrl);
+			$this->data->set('currentQueryUrl', $currentQueryUrl);
+		}
+	}
 
-    /**
-     * Exception handler
-     *
-     * @return Response
-     */
-    public function handleException() {
-        $e = $this->request->get('exception');
+	/**
+	 * Exception handler
+	 *
+	 * @return Response
+	 */
+	public function handleException() {
+		$e = $this->request->get('exception');
 
-        if (empty($e) || ! $e instanceof FlattenException) $e = new FlattenException();
+		if (empty($e) || ! $e instanceof FlattenException) $e = new FlattenException();
 
-        $this->layout = 'modules/error/general.tpl';
-        $data = ModelBase::factory('Template')->getDefaultData();
+		$this->layout = 'modules/error/general.tpl';
+		$data = ModelBase::factory('Template')->getDefaultData();
 
-        // Additional setter
-        $this->data->set('title', 'Kesalahan');
-        $this->data->set('content', $e);
+		// Additional setter
+		$this->data->set('title', 'Kesalahan');
+		$this->data->set('content', $e);
 
-        return $this->render($data);
-    }
+		return $this->render($data);
+	}
 
-    /**
-     * API untuk Session
-     *
-     * @return Session
-     */
-    public function getSession() {
-        return $this->session;
-    }
+	/**
+	 * API untuk Session
+	 *
+	 * @return Session
+	 */
+	public function getSession() {
+		return $this->session;
+	}
 
-    /**
-     * API untuk ACL
-     *
-     * @return Acl
-     */
-    public function getAcl() {
-        return $this->acl;
-    }
+	/**
+	 * API untuk ACL
+	 *
+	 * @return Acl
+	 */
+	public function getAcl() {
+		return $this->acl;
+	}
 
-    /**
-     * API untuk Data
-     *
-     * @return Parameter
-     */
-    public function getData() {
-        return $this->data;
-    }
+	/**
+	 * API untuk Data
+	 *
+	 * @return Parameter
+	 */
+	public function getData() {
+		return $this->data;
+	}
 
-    /**
-     * Redirect response.
-     *
-     * @param string $url URL Path
-     * @param int $status HTTP status code
-     * @param Array $headers HTTP Headers
-     *
-     * @return RedirectResponse A Response instance
-     */
-    public function redirect($url = '', $status = 302, $headers = array()) {
-        return new RedirectResponse($url, $status, $headers);
-    }
+	/**
+	 * Redirect response.
+	 *
+	 * @param string $url URL Path
+	 * @param int $status HTTP status code
+	 * @param Array $headers HTTP Headers
+	 *
+	 * @return RedirectResponse A Response instance
+	 */
+	public function redirect($url = '', $status = 302, $headers = array()) {
+		return new RedirectResponse($url, $status, $headers);
+	}
 
-    /**
-     * Render response.
-     *
-     * @param mixed $content A Response content
-     * @param int $status HTTP status code
-     * @param Array $headers HTTP Headers
-     *
-     * @return Response A Response instance
-     */
-    public function render($data = NULL, $status = 200, $headers = array()) {
-        // Persiapkan data yang akan di-render
-        if (is_array($data) && array_key_exists('title', $data) && array_key_exists('content', $data)) {
-            // Data yang dikirim memiliki parameter minimal yakni 'title' dan 'content'
-            // sertakan semua global data object yang mungkin diperlukan
-            $data = array_merge($data, $this->data->all());
+	/**
+	 * Render response.
+	 *
+	 * @param mixed $content A Response content
+	 * @param int $status HTTP status code
+	 * @param Array $headers HTTP Headers
+	 *
+	 * @return Response A Response instance
+	 */
+	public function render($data = NULL, $status = 200, $headers = array()) {
+		// Persiapkan data yang akan di-render
+		if (is_array($data) && array_key_exists('title', $data) && array_key_exists('content', $data)) {
+			// Data yang dikirim memiliki parameter minimal yakni 'title' dan 'content'
+			// sertakan semua global data object yang mungkin diperlukan
+			$data = array_merge($data, $this->data->all());
 
-            // yang diperlukan Twig. Load template yang berkaitan dan assign data.
-            $twigLoader = new \Twig_Loader_Filesystem(APPLICATION_PATH . DIRECTORY_SEPARATOR . 'Templates');
-            $twig = new \Twig_Environment($twigLoader);
-            $content = $twig->render($this->layout, $data);
-        } else {
-            // Data yang dikirim tidak memenuhi kriteria
-            // Outputkan raw data
-            $content = $data;
-        }
+			// Panggil render method
+			$content = ModelBase::factory('Template')->render($this->layout, $data);
+		} else {
+			// Data yang dikirim tidak memenuhi kriteria
+			// Outputkan raw data
+			$content = $data;
+		}
 
-        return new Response($content, $status, $headers);
-    }
+		return new Response($content, $status, $headers);
+	}
 
 }
--- app/Model/ModelTemplate.php
+++ app/Model/ModelTemplate.php
@@ -8,6 +8,9 @@
 
 namespace app\Model;
 
+use \Twig_Loader_Filesystem;
+use \Twig_Environment;
+
 /**
  * ModelTemplate
  *
@@ -15,94 +18,110 @@ namespace app\Model;
  */
 class ModelTemplate extends ModelBase 
 {
-	protected $defaultData = array(
-		'title'	=> 'Unknown Error',
-		'content' => 'Ada yang salah. Harap hubungi administrator.',
-		'menu_top' => array(
-			array('title' => 'Home', 'link' => '/'),
-			array('title' => 'Masuk', 'link' => '/auth/login'),
-			array('title' => 'Daftar', 'link' => '/auth/register'),
-		),
-		'menu_bottom' => array(),
-	);
+    protected $defaultData = array(
+        'title' => 'Unknown Error',
+        'content' => 'Ada yang salah. Harap hubungi administrator.',
+        'menu_top' => array(
+            array('title' => 'Home', 'link' => '/'),
+            array('title' => 'Masuk', 'link' => '/auth/login'),
+            array('title' => 'Daftar', 'link' => '/auth/register'),
+        ),
+        'menu_bottom' => array(),
+    );
 
-	/**
-	 * Provider untuk template Home
-	 *
-	 * @param array $otherData Data dari model lain
-	 *
-	 * @return array $finalData
-	 * @see ModelTemplate::finalData
-	 */
-	public function getHomeData($otherData = array()) {
-		$data = array(
-			'title'	=> 'Home',
-			'content' => NULL,
-		);
+    /**
+     * Render data ke template via Twig
+     *
+     * @param string $template eg:layout.tpl
+     * @param array $data 
+     *
+     * @return string HTML representation
+     */
+    public static function render($template, $data) {
+        // Inisialisasi Twig. Load template yang berkaitan dan assign data.
+        $loader = new Twig_Loader_Filesystem(APPLICATION_PATH . DIRECTORY_SEPARATOR . 'Templates');
+        $templateEngine = new Twig_Environment($loader);
+        
+        return $templateEngine->render($template, $data);
+    }
 
-		return $this->prepareData($data, $otherData);
-	}
+    /**
+     * Provider untuk template Home
+     *
+     * @param array $otherData Data dari model lain
+     *
+     * @return array $finalData
+     * @see ModelTemplate::finalData
+     */
+    public function getHomeData($otherData = array()) {
+        $data = array(
+            'title' => 'Home',
+            'content' => NULL,
+        );
 
-	/**
-	 * Provider untuk template User
-	 *
-	 * @param array $otherData Data dari model lain
-	 *
-	 * @return array $finalData
-	 * @see ModelTemplate::finalData
-	 */
-	public function getUserData($otherData = array()) {
-		$data = array(
-			'title'	=> 'Pengguna',
-			'content' => NULL,
-		);
+        return $this->prepareData($data, $otherData);
+    }
 
-		return $this->prepareData($data, $otherData);
-	}
+    /**
+     * Provider untuk template User
+     *
+     * @param array $otherData Data dari model lain
+     *
+     * @return array $finalData
+     * @see ModelTemplate::finalData
+     */
+    public function getUserData($otherData = array()) {
+        $data = array(
+            'title' => 'Pengguna',
+            'content' => NULL,
+        );
 
-	/**
-	 * Provider untuk template Auth
-	 *
-	 * @param array $otherData Data dari model lain
-	 *
-	 * @return array $finalData
-	 * @see ModelTemplate::finalData
-	 */
-	public function getAuthData($otherData = array()) {
-		$data = array();
+        return $this->prepareData($data, $otherData);
+    }
 
-		return $this->prepareData($data, $otherData);
-	}
+    /**
+     * Provider untuk template Auth
+     *
+     * @param array $otherData Data dari model lain
+     *
+     * @return array $finalData
+     * @see ModelTemplate::finalData
+     */
+    public function getAuthData($otherData = array()) {
+        $data = array();
 
-	/**
-	 * Mendapat default data
-	 *
-	 * @return array Default data
-	 */
-	public function getDefaultData() {
-		return $this->defaultData;
-	}
+        return $this->prepareData($data, $otherData);
+    }
 
-	/**
-	 * PrepareData
-	 *
-	 * @param array $data Data default tiap section
-	 * @param array $otherData Data dari model lain
-	 *
-	 * @return array $finalData
-	 */
-	protected function prepareData($data = array(), $otherData = array()) {
-		$finalData = $this->defaultData;
+    /**
+     * Mendapat default data
+     *
+     * @return array Default data
+     */
+    public function getDefaultData() {
+        return $this->defaultData;
+    }
 
-		// Hanya merge jika terdapat data
-		if ( ! empty ($data)) {
-			$finalData = array_merge($finalData,$data);
-		}
+    /**
+     * PrepareData
+     *
+     * @param array $data Data default tiap section
+     * @param array $otherData Data dari model lain
+     *
+     * @return array $finalData
+     */
+    protected function prepareData($data = array(), $otherData = array()) {
+        $finalData = $this->defaultData;
 
-		if ( ! empty ($otherData)) {
-			$finalData = array_merge($finalData, $otherData);
-		}
+        // Hanya merge jika terdapat data
+        if ( ! empty ($data)) {
+            $finalData = array_merge($finalData,$data);
+        }
 
-		return $finalData;
-	}
+        if ( ! empty ($otherData)) {
+            $finalData = array_merge($finalData, $otherData);
+        }
+
+        return $finalData;
+    }
 }
